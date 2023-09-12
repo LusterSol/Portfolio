1b4e9a2091abb624fdf0f6f0f01591051061f314
Luster <ribera.sebastian@gmail.com>
2023-09-12 19:35:13 -0400

    added some details and js

---
+++ resources/js/script.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+
+  function function2() {
+    var y = document.getElementById("DIV2");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+  }
+
+  function function3() {
+    var z = document.getElementById("DIV3");
+    if (z.style.display === "none") {
+        z.style.display = "block";
+    } else {
+        z.style.display = "none";
+    }
+  }
